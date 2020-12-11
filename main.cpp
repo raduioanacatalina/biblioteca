a756a5b7705cbd91c88317d27cdb8dc1af3f7e5b
Catalina <catalina_ric@yahoo.com>
2020-12-11 11:35:18 +0200

    try catch fixed, bits

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include "ExceptieCarteImprumutata.h"
 #include <utility>
 #include <memory>
-#include <bits/unique_ptr.h>
+
 
 // Clasa biblioteca(vector nr_carti), clasa Manuscris (nume, id)
 // pentru tema 2 -> mostenire: tip de carti
@@ -23,40 +23,20 @@ int main() {
     ptr->afisare();
 //    b.sortare_id();
 
-
-
-    Manuscris* manuscrisImprumutat = nullptr;
     std::string titlu = "Metro2033";
     try {
-        manuscrisImprumutat = ptr->imprumuta(titlu);
+         Manuscris* manuscrisImprumutat = nullptr;
+
+         manuscrisImprumutat = ptr->imprumuta(titlu);
+         manuscrisImprumutat = ptr->imprumuta(titlu);
+         titlu = "Three body problem";
+         manuscrisImprumutat = ptr->imprumuta(titlu);
     }catch(ExceptieTitluInexistent e){
         std::cout <<"Nu putem imprumuta cartea (nu exista in biblioteca) " << titlu << std::endl;
     }catch(ExceptieCarteImprumutata e){
         std::cout <<"Nu putem imprumuta cartea (cartea a fost deja imprumutata) " << titlu << std::endl;
     }
 
-    try {
-        manuscrisImprumutat = ptr->imprumuta(titlu);
-    }catch(ExceptieTitluInexistent e){
-        std::cout <<"Nu putem imprumuta cartea (nu exista in biblioteca) " << titlu << std::endl;
-    }catch(ExceptieCarteImprumutata e){
-        std::cout <<"Nu putem imprumuta cartea (cartea a fost deja imprumutata) " << titlu << std::endl;
-    }
-
-
-    titlu = "Three body problem";
-    std::cout <<"HERE"<<std::endl;
-    try {
-        manuscrisImprumutat = ptr->imprumuta(titlu);
-    }catch(ExceptieTitluInexistent e){
-        std::cout <<"Nu putem imprumuta cartea (nu exista in biblioteca) " << titlu << std::endl;
-    }catch(ExceptieCarteImprumutata e){
-        std::cout <<"Nu putem imprumuta cartea (cartea a fost deja imprumutata) " << titlu << std::endl;
-    }
-    std::cout <<"HERE 2"<<std::endl;
-
-//    std::cout << " NUME M I: " <<  manuscrisImprumutat->getNume() <<" " << manuscrisImprumutat->getAnAparitie() << std::endl;
-//    Manuscris* manuscrisImprumutat2 = b.imprumuta("Metro2033");
 
     Carte c1;
     Autor a1; // Autor* a1 = new Autor();
